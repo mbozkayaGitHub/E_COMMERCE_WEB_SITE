0b0db119bc5078b92de0d3878884bfc9d45bc9cb
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-12-15 18:56:37 +0300

    RouterStructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-  <div>deneme</div>
-  )
+    <div>
+      <Router>
+  
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
