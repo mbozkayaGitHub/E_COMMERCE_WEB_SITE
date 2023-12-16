248fcf181a3d06bdfed409afdfca624a0821e815
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-12-16 12:18:10 +0300

    App.jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/navbar/Navbar";
+import PageContainer from "./containers/PageContainer";
 
 function App() {
   return (
     <div>
-      <Router>
-  
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <PageContainer>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </PageContainer>
     </div>
   );
 }
